ce2b19822c2011f4860d1a80c74beb4beff102b9
Ralph Gibson <ralphegibson@Ralph-Gibsons-iMac-2.local>
2021-03-08 22:02:33 -0800

    Created basic eightball by creating 2 files. Created src/EightBall.css and src/EightBall.js. Also modified App() in src/App.js.

---
+++ src/EightBall.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./EightBall.css";
+
+const EightBall = (props) => {
+  const [color, setColor] = useState("black");
+  const [message, setMessage] = useState("Think of a Question");
+
+  const chooseAnswer = () => {
+    const randIndex = Math.floor(Math.random() * props.answers.length);
+    const randAnswer = props.answers[randIndex];
+    setColor(randAnswer.color);
+    setMessage(randAnswer.msg);
+  };
+
+  return (
+    <>
+      <div style={{ backgroundColor: color }} className="EightBall" onClick={ chooseAnswer }>
+        <div className="EightBall-message">
+          <p>{ message }</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+EightBall.defaultProps = {
+  answers: [
+    { msg: "It is certain.", color: "green" },
+    { msg: "It is decidedly so.", color: "green" },
+    { msg: "Without a doubt.", color: "green" },
+    { msg: "Yes - definitely.", color: "green" },
+    { msg: "You may rely on it.", color: "green" },
+    { msg: "As I see it, yes.", color: "green" },
+    { msg: "Most likely.", color: "green" },
+    { msg: "Outlook good.", color: "green" },
+    { msg: "Yes.", color: "green" },
+    { msg: "Signs point to yes.", color: "goldenrod" },
+    { msg: "Reply hazy, try again.", color: "goldenrod" },
+    { msg: "Ask again later.", color: "goldenrod" },
+    { msg: "Better not tell you now.", color: "goldenrod" },
+    { msg: "Cannot predict now.", color: "goldenrod" },
+    { msg: "Concentrate and ask again.", color: "goldenrod" },
+    { msg: "Don't count on it.", color: "red" },
+    { msg: "My reply is no.", color: "red" },
+    { msg: "My sources say no.", color: "red" },
+    { msg: "Outlook not so good.", color: "red" },
+    { msg: "Very doubtful.", color: "red" }
+  ]
+};
+
+export default EightBall;
